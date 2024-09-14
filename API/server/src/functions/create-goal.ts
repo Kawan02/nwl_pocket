de5b34f4858e3f5e44bb29f6b39ea1d4c9abda49
Kawan02 <kawanng39@gmail.com>
2024-09-14 16:18:01 -0300

    first commit in project api

---
+++ API/server/src/functions/create-goal.ts
@@ -0,0 +1,26 @@
+import { db } from '../db'
+import { goals } from '../db/schema'
+
+interface CreateGoalRequest {
+  title: string
+  desiredWeekLyFrequency: number
+}
+
+export async function createGoal({
+  title,
+  desiredWeekLyFrequency,
+}: CreateGoalRequest) {
+  const result = await db
+    .insert(goals)
+    .values({
+      title,
+      desiredWeekLyFrequency,
+    })
+    .returning()
+
+  const goal = result[0]
+
+  return {
+    goal,
+  }
+}
